789896f085bf1fcf5f692e88422d06b36f8136e5
Vuk Arandjelovic <arandjelovicvuk@gmail.com>
2023-05-11 19:30:52 +0200

    Added working map to application

--- App.js
+++ App.js
@@ -2,20 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 
+//const APIKey = "AIzaSyA4cPpI8vEtuH7XsehicnxaJVlTGo2Cs4E"
+
+const styles2 = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    height: 700,
+    width: 300,
+  },
+ });
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text></Text>
-      <StatusBar style="auto" />
       <MapView
+        style={styles2.map}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 58.78825,
+          longitude: 58.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
       />  
+      <Text></Text>
+      <StatusBar style="auto" />
+      
     </View>
   );
 }
