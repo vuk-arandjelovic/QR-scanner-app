068766262c6622c4f14cfbc2bbbf704b03c1e57f
Vuk Arandjelovic <arandjelovicvuk@gmail.com>
2023-05-25 20:21:42 +0200

    Fixed some dependencies and made Camera work

--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -1,14 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// const ScannerScreen = () => {
+//   return (
+//     <View>
+//       <Text>ScannerScreen</Text>
+//     </View>
+//   )
+// }
+
+// export default ScannerScreen
+
+// const styles = StyleSheet.create({
+//   barcodeTextURL: {
+//     fontSize: 20,
+//     color: 'white',
+//     fontWeight: 'bold',
+//   },
+// });
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 const ScannerScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>ScannerScreen</Text>
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
-  )
+  );
 }
 
 export default ScannerScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  barcodeTextURL: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+});
