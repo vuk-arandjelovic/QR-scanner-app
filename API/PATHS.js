fa8044191e7c2fab9e2358932c6b3555f2046bc9
BoromisaNikola <boromisanikola2001@gmail.com>
2023-05-30 01:50:09 +0200

    Ispravke mape i url-a

--- API/PATHS.js
+++ API/PATHS.js
@@ -7,9 +7,11 @@ const PATH_VUK = `http://192.168.1.4:8000`
 // Putanja za API na mom MacBook-u (kod mn kuci)
 const PATH_VUK_MAC = `http://192.168.1.2:8000`
 // Putanja za API na Borovnicinom laptopu (kod mn kuci)
-const PATH_BOROVNICA = `http://192.168.1.17:8000`
+const PATH_BOROVNICA = `http://127.0.0.1:8000`
 // Putanja za API hostovan na Borovnicinom RaspberryPI serveru
 const PATH_RPI = `http://borovnica.ddns.net:8000`
+// Putanja za API hostovan na Borovnicinom server2 serveru
+const PATH_SERVER2 = `http://borovnica.ddns.net:10000`
 
 // const API_URL = PATH_LOCALHOST
-export const API_URL = PATH_VUK
+export const API_URL = PATH_SERVER2
